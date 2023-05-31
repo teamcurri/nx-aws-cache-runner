962fb697d6af88ee545386447844b547a8833ddb
Navid Kabir <navid113@gmail.com>
2023-05-30 17:01:30 -0700

    Update the code to use devkit instead of old defaultTaskRunner

--- src/tasks-runner/runner.ts
+++ src/tasks-runner/runner.ts
@@ -1,8 +1,7 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
 import { TaskStatus } from '@nx/workspace/src/tasks-runner/tasks-runner'
-import defaultTaskRunner from '@nx/workspace/tasks-runners/default'
+import { defaultTasksRunner } from '@nx/devkit'
 import { config as dotEnvConfig } from 'dotenv'
-import { from, Observable, Subject } from 'rxjs'
 
 import { AwsCache, AwsNxCacheOptions } from './aws-cache'
 import { Logger } from './logger'
@@ -24,9 +23,10 @@ function getOptions(options: AwsNxCacheOptions) {
 }
 
 export const tasksRunner = (
-  tasks: Parameters<typeof defaultTaskRunner>[0],
-  options: Parameters<typeof defaultTaskRunner>[1] & AwsNxCacheOptions,
-  context: Parameters<typeof defaultTaskRunner>[2]
+  tasks: Parameters<typeof defaultTasksRunner>[0],
+  options: Parameters<typeof defaultTasksRunner>[1] & AwsNxCacheOptions,
+  // eslint-disable-next-line no-magic-numbers
+  context: Parameters<typeof defaultTasksRunner>[2]
 ) => {
   const awsOptions: AwsNxCacheOptions = getOptions(options)
   const logger = new Logger()
@@ -35,43 +35,34 @@ export const tasksRunner = (
     if (process.env.NX_AWS_DISABLE === 'true') {
       logger.note('Using Local Cache (NX_AWS_DISABLE is set to true)')
 
-      return defaultTaskRunner(tasks, options, context)
+      return defaultTasksRunner(tasks, options, context)
     }
 
     logger.note('Using AWS S3 Remote Cache')
 
     const messages = new MessageReporter(logger)
     const remoteCache = new AwsCache(awsOptions, messages)
-    const runnerWrapper = new Subject<{ [id: string]: TaskStatus }>()
-    const runner = defaultTaskRunner(
+
+    const runner: Promise<{ [id: string]: TaskStatus }> = defaultTasksRunner(
       tasks,
       {
         ...options,
         remoteCache,
       },
       context
-    ) as Observable<{ [id: string]: TaskStatus }>
+    ) as Promise<{ [id: string]: TaskStatus }>
 
-    from(runner).subscribe({
-      next(value) {
-        return runnerWrapper.next(value)
-      },
-      error(err) {
-        return runnerWrapper.error(err)
-      },
-      async complete() {
-        await remoteCache.waitForStoreRequestsToComplete()
-        messages.printMessages()
-        runnerWrapper.complete()
-      },
+    runner.finally(async () => {
+      await remoteCache.waitForStoreRequestsToComplete()
+      messages.printMessages()
     })
 
-    return runnerWrapper.toPromise()
+    return runner
   } catch (err) {
     logger.warn((err as Error).message)
     logger.note('Using Local Cache')
 
-    return defaultTaskRunner(tasks, options, context)
+    return defaultTasksRunner(tasks, options, context)
   }
 }
 
